630c7a2de6ca2fca18a2321cc34e76516e80cf63
alamin500 <nalamin500@gmail.com>
2021-10-31 00:32:12 +0600

    commit

--- index.js
+++ index.js
@@ -7,16 +7,21 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
 const uri =
-  "mongodb+srv://khair:khair@cluster0.2vil1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://alamin-matlab_2021:gVZPplwupeaQWwOJ@cluster0.2vil1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.texip.mongodb.net/volunteerNetwork?retryWrites=true&w=majority`;
+
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 app.get("/", (req, res) => {
-  res.send("Welcome our tour booking gg ff Website!");
+  res.send("Welcome our tour booking Website!");
   console.log(req);
 });
 client.connect((err) => {
@@ -39,7 +44,7 @@ client.connect((err) => {
     console.log(result);
   });
 
-  // My books
+  // Get My books
   app.get("/myBooks/:email", async (req, res) => {
     const result = await BooksCollection.find({
       email: req.params.email,
@@ -47,8 +52,14 @@ client.connect((err) => {
     res.send(result);
   });
 
+  // Get All Books
+  app.get("/allBooks", async (req, res) => {
+    const result = await BooksCollection.find({}).toArray();
+    res.send(result);
+  });
+
   // Delete MyBook
-  app.delete("/deleteEvent/:id", async (req, res) => {
+  app.delete("/deleteBook/:id", async (req, res) => {
     console.log(req.params.id);
     const result = await BooksCollection.deleteOne({
       _id: req.params.id,
