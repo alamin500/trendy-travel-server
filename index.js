9ea242a2d21e6319d0d923f6a12a58a6daece2bf
alamin500 <nalamin500@gmail.com>
2021-10-31 18:24:34 +0600

    commit

--- index.js
+++ index.js
@@ -13,13 +13,14 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.texip.mongodb.net/volunteerNetwork?retryWrites=true&w=majority`;
 
 // const client = new MongoClient(uri, {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 // });
-
+console.log(uri);
 app.get("/", (req, res) => {
   res.send("Welcome our tour booking Website!");
   console.log(req);
